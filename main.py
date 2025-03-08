06af6d12cf813b0203780ed838540b36af8e8ba9
Audrius Pasvenskas <apasvenskas@outlook.com>
2025-03-07 21:04:42 -0800

    successfully getting title and description data

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ apiKey = "3763109844bb4720bcfd5dcca99b5a11"
 url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=3763109844bb4720bcfd5dcca99b5a11"
 
 request = requests.get(url)
-content = request.text
-print(content)
+content = request.json()
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
