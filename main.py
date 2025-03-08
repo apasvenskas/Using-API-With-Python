6dc024bef63257243b2de11e2e01e645e25f6588
Audrius Pasvenskas <apasvenskas@outlook.com>
2025-03-08 14:35:00 -0800

    added news link subject title and filter by eng to the email

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import requests
 from send_email import send_email
 
 apiKey = "3763109844bb4720bcfd5dcca99b5a11"
-url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=3763109844bb4720bcfd5dcca99b5a11"
+url = "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=3763109844bb4720bcfd5dcca99b5a11&languege=en"
 
 request = requests.get(url)
 content = request.json()
 
 body = ""
-for article in content["articles"]:
-   body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+for article in content["articles"][0:20]:
+   if article["title"] is not None:
+      body = "Subject: Today's news" + "\n" + body + article["title"] + \
+         "\n" + article["description"] + "\n" + article["url"] + 2*"\n" # Displaying the data and links
 
 body = body.encode("utf-8")
 print(body)
